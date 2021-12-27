e34513f3671a5c48ecf08292aed91a98fe071426
Closcaru Petrica <petryca.95@gmail.com>
2021-12-27 15:58:15 +0200

    Rezolvare bug modificare cantitati oferta

--- app/Http/Controllers/VoyagerOfferController.php
+++ app/Http/Controllers/VoyagerOfferController.php
@@ -1346,7 +1346,7 @@ class VoyagerOfferController extends \TCG\Voyager\Http\Controllers\VoyagerBaseCo
           })->first();
           $checkedParent->offerProducts = $offProd;
           $ronCuTVA = $checkedParent != null ? $checkedParent->ron_cu_tva : 0;
-          $ronTotal = $ronCuTVA*$parent->offerProducts->qty;
+          $ronTotal = $ronCuTVA*$checkedParent->offerProducts->qty;
           $totalCalculat += $ronTotal;
         }
       }
