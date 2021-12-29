fb6dc4bf97069161a082cc7ac5644f9fe1df83d9
george <george.petrescu@gmail.com>
2021-12-29 10:24:32 +0200

    bugfix

--- config/winmentor.php
+++ config/winmentor.php
@@ -5,7 +5,7 @@ return [
     'host' => env('WIN_MENTOR_HOST', '127.0.0.1'),
     'port' => env('WIN_MENTOR_PORT', '1234'),
 
-    'states ' => [
+    'states' => [
         'Bistrita-Nasaud' => 'Bistrita Nasaud',
         'Caras-Severin' => 'Caras Severin',
         'Satu Mare' => 'Satu-Mare',
