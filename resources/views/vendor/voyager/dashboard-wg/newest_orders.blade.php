50daa4d5de322248564aeaa3d907ddfe8aa1c44c
george <george.petrescu@gmail.com>
2022-01-13 22:06:11 +0200

    dashboard - updates

--- resources/views/vendor/voyager/dashboard-wg/newest_orders.blade.php
+++ resources/views/vendor/voyager/dashboard-wg/newest_orders.blade.php
@@ -7,9 +7,9 @@
   @if($top5Orders && count($top5Orders) > 0)
     @foreach($top5Orders as $order)
       <div class="nw__details">
-        <a href="/admin/offers/{{$order->id}}/edit" class="nw__orderNr">{{$order->id . $order->numar_comanda ? "(C{$order->numar_comanda}" : ""}}</a>
+        <a href="/admin/offers/{{$order->id}}/edit" class="nw__orderNr">{{$order->id." ".($order->numar_comanda ? "(C{$order->numar_comanda}" : "")}}</a>
         <span class="nw__clientName">{{$order->client->name ?? ''}}</span>
-        <span class="nw__price"> {{number_format($order->total_final, 2, '.', '')}} RON </span>
+        <span class="nw__price"> {{number_format($order->total_final, 2, ',', '.')}} RON </span>
         <span class="nw__date"> {{\Carbon\Carbon::parse($order->offer_date)->format('d F')}} </span>
       </div>
     @endforeach
