1a9fff4cce2781d0a76c08ee5e52a2a18ec839e8
george <george.petrescu@gmail.com>
2022-01-24 20:01:34 +0200

    wme get products updates

--- app/Console/Commands/WinMentor.php
+++ app/Console/Commands/WinMentor.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 use App\Product;
 use App\Unit;
 
-class WinMentor extends Command
+class pWinMentor extends Command
 {
     /**
      * The name and signature of the console command.
@@ -39,63 +39,73 @@ class WinMentor extends Command
      */
     public function handle()
     {
-      $insertProducts = [];
-      $counterInsert = 0;
-      $counterUpdate = 0;
-      // url final pentru preluare produse din mentor "pe incredere" fara token sau metoda de autentificare, momentan
-      $url = "http://".config('winmentor.host').":".config('winmentor.port')."/datasnap/rest/TServerMethods/%22GetInfoArticole%22/";
-      //  Initiate curl
-      $ch = curl_init();
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_URL,$url);
-      $result=curl_exec($ch);
-      curl_close($ch);
-      $winMentorFetch = json_decode($result, true);
-      // daca am produse, atunci le iau din InfoArticole
-      if(count($winMentorFetch) > 0 && $winMentorFetch['result'] == "ok"){
-        $winMentorProducts = $winMentorFetch['InfoArticole'];
-      }
-      $createdAt = date('Y-m-d H:i:s');
-      foreach($winMentorProducts as $key => $product){
-        $checkProduct = Product::where('mentor_cod_intern', $product['CodIntern'])->first();
-        if($checkProduct != null){
-          $checkProduct->name = $product['Denumire'];
-          $checkProduct->status = $product['Inactiv'] == "Da" ? 0 : 1;
-          $checkProduct->mentor_cod_obiect = $product['CodObiect'];
-          $checkProduct->mentor_description = null;
-          $checkProduct->mentor_cod_intern = $product['CodIntern'];
-          $checkProduct->created_at = $createdAt;
-          $checkProduct->updated_at = $createdAt;
-          $checkProduct->mentor_response = json_encode($product);
-          $checkProduct->save();
-          $counterUpdate++;
-        } else{
-          array_push($insertProducts,
-          [
-            'name' => $product['Denumire'],
-            'status' => $product['Inactiv'] == "Da" ? 0 : 1,
-            'mentor_cod_obiect' => $product['CodObiect'],
-            'mentor_description' => null,
-            'mentor_cod_intern' => $product['CodIntern'],
-            'created_at' => $createdAt,
-            'updated_at' => $createdAt,
-            'mentor_response' => json_encode($product),
-          ]);
-          $counterInsert++;
+        $insertProducts = [];
+        $counterInsert = 0;
+        $counterUpdate = 0;
+        // url final pentru preluare produse din mentor "pe incredere" fara token sau metoda de autentificare, momentan
+        $url = "http://" . config('winmentor.host') . ":" . config(
+                'winmentor.port'
+            ) . "/datasnap/rest/TServerMethods/%22GetInfoArticole%22/";
+        //  Initiate curl
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $winMentorFetch = json_decode($result, true);
+        // daca am produse, atunci le iau din InfoArticole
+        if (count($winMentorFetch) > 0 && $winMentorFetch['result'] == "ok") {
+            $winMentorProducts = $winMentorFetch['InfoArticole'];
         }
-        $unit = Unit::where('title', $product['UM'])->first();
-        if($unit == null){
-          $unit = new Unit;
-          $unit->title = $product['UM'];
-          $checkProduct->created_at = $createdAt;
-          $checkProduct->updated_at = $createdAt;
-          $unit->save();
+        $createdAt = date('Y-m-d H:i:s');
+        foreach ($winMentorProducts as $key => $product) {
+            $checkProduct = Product::where('mentor_cod_extern', $product['CodExtern'])->first();
+            if ($checkProduct != null) {
+                //momentan nu facem nimic cu produsele care sunt deja bagate
+//                $checkProduct->name = $product['Denumire'];
+//                $checkProduct->status = $product['Inactiv'] == "Da" ? 0 : 1;
+//                $checkProduct->mentor_cod_obiect = $product['CodExtern'];
+//                $checkProduct->mentor_description = null;
+//                $checkProduct->mentor_cod_intern = $product['CodIntern'];
+//                $checkProduct->mentor_cod_extern = $product['CodExtern'];
+//                $checkProduct->created_at = $createdAt;
+//                $checkProduct->updated_at = $createdAt;
+//                $checkProduct->mentor_response = json_encode($product);
+//                $checkProduct->save();
+//                $counterUpdate++;
+            } else {
+                array_push(
+                    $insertProducts,
+                    [
+                        'name' => $product['Denumire'],
+                        'status' => $product['Inactiv'] == "Da" ? 0 : 1,
+                        'mentor_cod_obiect' => $product['CodObiect'],
+                        'mentor_description' => null,
+                        'mentor_cod_intern' => $product['CodIntern'],
+                        'mentor_cod_extern' => $product['CodExtern'],
+                        'created_at' => $createdAt,
+                        'updated_at' => $createdAt,
+                        'mentor_response' => json_encode($product),
+                    ]
+                );
+                $counterInsert++;
+            }
+            $unit = Unit::where('title', $product['UM'])->first();
+            if ($unit == null) {
+                $unit = new Unit;
+                $unit->title = $product['UM'];
+                $checkProduct->created_at = $createdAt;
+                $checkProduct->updated_at = $createdAt;
+                $unit->save();
+            }
         }
-      }
-      $insertion = null;
-      if(count($insertProducts) > 0){
-        $insertion = Product::insert($insertProducts);
-      }
-      return ['success' => true, 'msg' => "Au fost adaugate ".$counterInsert." produse si modificare ". $counterUpdate." produse."];
+        $insertion = null;
+        if (count($insertProducts) > 0) {
+            $insertion = Product::insert($insertProducts);
+        }
+        return [
+            'success' => true,
+            'msg' => "Au fost adaugate " . $counterInsert . " produse si modificare " . $counterUpdate . " produse."
+        ];
     }
 }
