feef279de54ff028e9148d9fcebfa2140ed59af2
george <george.petrescu@gmail.com>
2022-03-08 21:36:25 +0200

    offer edit - increase font size

--- resources/views/vendor/voyager/products/offer_box.blade.php
+++ resources/views/vendor/voyager/products/offer_box.blade.php
@@ -59,17 +59,17 @@
             <td style="text-align:center">
               <input type="number"
                 @if($parent->offerProducts != null) name="offerQty[]" value="{{$parent->offerProducts->qty}}" {!! isset($onlySelectQty) && $onlySelectQty ? 'tabindex="'.(++$tabindex).'"' : '' !!} @else readonly @endif
-                autocomplete="off" class="form-control input-sm changeQty parentId-{{$parent->id}}" parentId="{{$parent->id}}" style="width: 70px; display:inline"
+                autocomplete="off" class="form-control input-sm changeQty parentId-{{$parent->id}}" parentId="{{$parent->id}}" style="width: 70px; display:inline; color: black; font-size: 14px"
               />
             </td>
             <td style="text-align:center;">
-              <input readonly type="number" autocomplete="off" class="form-control input-sm eurFaraTVA parent-{{$parent->id}}" style="width: 70px; display:inline; cursor: not-allowed;" @if($eurFaraTVA != 0) value="{{$eurFaraTVA}}" @else value="0.00" @endif>
+              <input readonly type="number" autocomplete="off" class="form-control input-sm eurFaraTVA parent-{{$parent->id}}" style="width: 70px; display:inline; cursor: not-allowed; color: black; font-size: 14px" @if($eurFaraTVA != 0) value="{{$eurFaraTVA}}" @else value="0.00" @endif>
             </td>
             <td style="text-align:center;">
-              <input readonly type="number" class="form-control input-sm ronCuTVA parent-{{$parent->id}}" style="width: 70px; display:inline; cursor: not-allowed;" @if($ronCuTVA != 0) value="{{$ronCuTVA}}" @else value="0.00" @endif>
+              <input readonly type="number" class="form-control input-sm ronCuTVA parent-{{$parent->id}}" style="width: 70px; display:inline; cursor: not-allowed; color: black; font-size: 14px" @if($ronCuTVA != 0) value="{{$ronCuTVA}}" @else value="0.00" @endif>
             </td>
             <td style="text-align:center;">
-              <input readonly type="number" class="form-control input-sm ronTotal parent-{{$parent->id}}" style="width: 70px; display:inline; cursor: not-allowed;" @if($ronCuTVA != 0) value="{{$ronTotal}}" @else value="0.00" @endif>
+              <input readonly type="number" class="form-control input-sm ronTotal parent-{{$parent->id}}" style="width: 70px; display:inline; cursor: not-allowed; color: black; font-size: 14px" @if($ronCuTVA != 0) value="{{$ronTotal}}" @else value="0.00" @endif>
             </td>
             <td style="background: lightgrey"></td>
             <td style="text-align: center;">
